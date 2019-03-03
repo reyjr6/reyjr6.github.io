c3d88d4dfe1de19bdf23f42ebc6af7b06308458f
Reynaldo Olivo <rey@Reynaldos-MacBook-Pro.local>
2019-03-03 16:36:05 -0500

    new build and server deployment

---
+++ build/precache-manifest.0fdf8ecc3b91ec4093a33cd245d5f618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fbbe45c50c5ad71b1163",
+    "url": "/static/js/main.fbbe45c5.chunk.js"
+  },
+  {
+    "revision": "4b95350b2236d8ff47ed",
+    "url": "/static/js/2.4b95350b.chunk.js"
+  },
+  {
+    "revision": "fbbe45c50c5ad71b1163",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "109801f4968cdb00c771d17b45d3b95b",
+    "url": "/index.html"
+  }
+];
