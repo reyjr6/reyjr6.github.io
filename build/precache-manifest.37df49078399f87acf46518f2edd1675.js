0e1ede01b98c40ca97fd8369d5ed539dd843dc1c
reyjr6 <rey@Reynaldos-MacBook-Pro.local>
2019-03-03 20:46:46 -0500

    actual logic commit

---
+++ build/precache-manifest.37df49078399f87acf46518f2edd1675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2732e3ff263647d567bd",
+    "url": "/static/js/main.2732e3ff.chunk.js"
+  },
+  {
+    "revision": "4b95350b2236d8ff47ed",
+    "url": "/static/js/2.4b95350b.chunk.js"
+  },
+  {
+    "revision": "2732e3ff263647d567bd",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "a2f7c83d63684746729610b0aff31302",
+    "url": "/index.html"
+  }
+];
